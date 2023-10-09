0faaf0758744b76e891707e12c46254ad307b45e
surendra <surendra.chakali@hyniva.com>
2023-10-09 12:27:31 +0530

    table name changed

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
-const { getBankDetails, getAllBanks } = require("./api");
+const { getBankDetails, getAllBanks, createEmployeeBankDetails } = require("./api");
 
 describe("positive getBankDetails", () => {
   let originalSend;
@@ -128,3 +128,87 @@ describe("Get the all bank details of employees ", () => {
     });
   });
 });
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// Mock DynamoDBClient to avoid making actual AWS calls
+const mockClient = {
+  send: () => ({}),
+};
+// Mock employee data for createEmployee
+const createEmployeeData = {
+    postId: "3",
+    bankDetails: {
+      BankName: "ka",
+      BranchName: "hydrabad",
+      BranchAddress: "bangalore",
+      CustomerNumber: "12345678912",
+      BankAccountNumber: "55566444412",
+      IsSalaryAccount: "yes",
+      IsActive: "yes",
+      IsDeleted: "false"
+    }
+  };
+// Mock employee data for updateEmployee
+const updateEmployeeData = {
+    BankName: "kanara",
+    BranchName: "hydrabad",
+    BranchAddress: "bangalore",
+    CustomerNumber: "12345678912",
+    BankAccountNumber: "55566444412",
+    IsSalaryAccount: "yes",
+    IsActive: "yes",
+    IsDeleted: "flase"
+};
+// Successfully create an employee
+// Successfully create an employee
+describe('createEmployee unit tests', () => {
+    let originalDynamoDBClient;
+    before(() => {
+      originalDynamoDBClient = DynamoDBClient;
+      DynamoDBClient.prototype.send = () => mockClient.send();
+    });
+    after(() => {
+      DynamoDBClient.prototype.send = originalDynamoDBClient.prototype.send;
+    });
+    it('successfully create an employee', async () => {
+      // Mock event object with employee data
+      let event = {
+        body: JSON.stringify(createEmployeeData),
+      };
+      const response = await createEmployeeBankDetails(event);
+      expect(response.statusCode).to.equal(200);
+      const responseBody = JSON.parse(response.body);
+      expect(responseBody.message).to.equal('Successfully created post.'); // Correct the message if necessary
+    });
+    it('fails to create an employee with missing data', async () => {
+      // Mock event object with missing data
+      let event = {
+        body: JSON.stringify({}), // Missing required data
+      };
+      const response = await createEmployeeBankDetails(event);
+      expect(response.statusCode).to.equal(500); // Expecting an error response
+    });
+    it('fails to create an employee with invalid data', async () => {
+      // Mock event object with invalid data
+      let event = {
+        // body: JSON.stringify({
+        //   // Invalid data that should fail validation
+        //   BankName: 'AB', // Too short
+        // }),
+        body: JSON.stringify(createEmployeeData),
+      };
+      const response = await createEmployeeBankDetails(event);
+      expect(response.statusCode).to.equal(500); // Expecting an error response
+      //const responseBody = JSON.parse(response.body);
+      //expect(responseBody.message).to.equal('BankName should be minimum 3 characters!'); // Correct the message if necessary
+      expect(JSON.parse(response.body)).to.deep.equal({
+        message: "BankName should be minimum 3 characters!",
+      });
+    });
+  });
+
+  /////////////////////////////////////////////////////////////////////////////////////////
