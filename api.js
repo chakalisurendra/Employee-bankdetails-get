050e1fcc294296cb90691cd3c1158bc2fe183d84
surendra <surendra.chakali@hyniva.com>
2023-10-09 12:16:06 +0530

    create employee details

--- api.js
+++ api.js
@@ -3,6 +3,7 @@ const {
   DynamoDBClient, // Dynamodb instance
   GetItemCommand, // Retrieve data fron dynamoDb table
   ScanCommand, // Scan the table
+  PutItemCommand,
 } = require("@aws-sdk/client-dynamodb"); //aws-sdk is used to build rest APIs
 const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb"); // Importing marshall, unmarshall for Convert a JavaScript object into a DynamoDB record and a DynamoDB record into a JavaScript object
 const client = new DynamoDBClient(); // Create new instance of DynamoDBClient to client, will use this constant across the program
@@ -77,7 +78,114 @@ const getAllBanks = async () => {
   return response; //Return response with statusCode and data.
 };
 
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// Define regular expressions for validation
+const nameRegex = /^[A-Za-z]{3,32}$/;
+const CustomerNumberRegex = /^\d{11,12}$/;
+const BankAccountNumber = /^\d{11,16}$/;
+// Validation function for bankDetails object
+const validation = (bankDetails) => {
+  if (!nameRegex.test(bankDetails.BankName)) {
+    return "BankName should be minimum 3 characters!";
+  }
+  if (!nameRegex.test(bankDetails.BranchName)) {
+    return "BranchName should be minimum 3 characters!";
+  }
+  if (!nameRegex.test(bankDetails.BranchAddress)) {
+    return "BranchAddress should be minimum 3 characters!";
+  }
+  if (!CustomerNumberRegex.test(bankDetails.CustomerNumber)) {
+    return "CustomerNumber should be minimum 11 characters!";
+  }
+  if (!BankAccountNumber.test(bankDetails.BankAccountNumber)) {
+    return "BankAccountNumber should be minimum 11 digits!";
+  }
+  //return null; // Validation passed
+};
+// Function to create an employee
+const createEmployeeBankDetails = async (event) => {
+  const response = { statusCode: 200 };
+  try {
+    // Parse the JSON body from the event
+    const body = JSON.parse(event.body);
+    const bankDetails = body.bankDetails;
+    console.log(bankDetails);
+    // Perform validation on bankDetails
+    const validationError = validation(bankDetails);
+    if (validationError) {
+      // console.log("CustomerNumber:", bankDetails.CustomerNumber);
+      // response.statusCode =500;
+      // response.body=JSON.stringify({
+      //   message: validationError,
+      // })
+      response.statusCode = 404; // Setting the status code to 404
+      response.body = JSON.stringify({
+        message: validationError,
+      });
+      //throw new Error(validationError);
+    }
+    // Check for required fields in the body
+    // if (!body.bankDetails.BankName || !body.bankDetails.BranchName || !body.bankDetails.BranchAddress || !body.bankDetails.BankAccountNumber) {
+    //   throw new Error('Required fields are missing.');
+    // }
+    // Fetch an item from DynamoDB based on postId
+    const employeeData = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Key: marshall({ postId: body.postId }),
+    };
+    const { Item } = await client.send(new GetItemCommand(employeeData));
+    // Check if an item with the same postId exists in DynamoDB
+    if (Item) {
+      const item1 = { item2: Item ? unmarshall(Item) : {} };
+      console.log(item1);
+      // Check if bankDetails already exist in the fetched item
+      if (item1.item2.bankDetails) {
+        throw new Error("BankDetails already exists!");
+      }
+    }
+    // Define parameters for inserting an item into DynamoDB
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Item: marshall(
+        {
+          empId: body.empId,
+          bankDetails: {
+            BankName: bankDetails.BankName,
+            BranchName: bankDetails.BranchName,
+            BranchAddress: bankDetails.BranchAddress,
+            CustomerNumber: bankDetails.CustomerNumber,
+            BankAccountNumber: bankDetails.BankAccountNumber,
+            IsSalaryAccount: bankDetails.IsSalaryAccount,
+            IsActive: bankDetails.IsActive,
+            IsDeleted: bankDetails.IsDeleted,
+          },
+        },
+        { removeUndefinedValues: true }
+      ),
+    };
+    // Insert the item into DynamoDB
+    await client.send(new PutItemCommand(params));
+    response.body = JSON.stringify({
+      message: "Successfully created post.",
+    });
+    //To through the exception if anything failing while creating bankDetails
+  } catch (e) {
+    console.error(e);
+    response.body = JSON.stringify({
+      message: 'Failed to create BankDetails',
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
+  }
+  return response;
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 module.exports = {
   getBankDetails,
   getAllBanks,
+  createEmployeeBankDetails,
 }; // exporting the function
